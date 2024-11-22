5ff3bc7bbe40495798314e4fe52c0118cc64cd49
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-11-22 17:08:13 -0300

    Continuando a estrutura

--- estrutura.cpp
+++ estrutura.cpp
@@ -4,16 +4,22 @@
 
 using namespace std;
 
-
+// Estrutura do imóvel
 typedef struct Imovel{
+    // Distância até o final do segmento a que pertence
+    int dFinalSeg;
     int CEP;
     string rua;
     int num;
     string tipo;    
 } imovel;
 
+// Estrutura do segmento (aresta do grafo)
 typedef struct segmento{
-    int id;
+    // Vértices de onde ele sai e entra
+    int vSaida;
+    int vEntrada;
+    // Vetor dos imóveis
     vector<imovel> imoveis;
     int limVel;
     int tamanho;
@@ -21,12 +27,12 @@ typedef struct segmento{
 } segmento;
 
 typedef struct planta{
+    // A lista de adjacência é um vetor de vetores de segmentos
+    // Cada entrada i do vetor externo corresponde às arestas de saída do vértice i
     vector<vector<segmento>> listaAdj;
 } planta;
 
 int main(){
-    vector<segmento> todosSegs;
-
     return 0;
 }
 
