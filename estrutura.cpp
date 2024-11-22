8429109d0e3cf292072ae46173ffa2735ff98a9a
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-11-22 17:29:39 -0300

    Funções de criação das estruturas

--- estrutura.cpp
+++ estrutura.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+
+using std::cout;
+using std::string;
+using std::endl;
 
 using namespace std;
 
@@ -12,25 +17,63 @@ typedef struct Imovel{
     string rua;
     int num;
     string tipo;    
-} imovel;
+} Imovel;
 
 // Estrutura do segmento (aresta do grafo)
-typedef struct segmento{
+typedef struct Segmento{
     // Vértices de onde ele sai e entra
     int vSaida;
     int vEntrada;
     // Vetor dos imóveis
-    vector<imovel> imoveis;
+    vector<Imovel> imoveis;
     int limVel;
     int tamanho;
     string rua;
-} segmento;
+} Segmento;
 
-typedef struct planta{
+typedef struct Planta{
     // A lista de adjacência é um vetor de vetores de segmentos
     // Cada entrada i do vetor externo corresponde às arestas de saída do vértice i
-    vector<vector<segmento>> listaAdj;
-} planta;
+    vector<vector<Segmento>> listaAdj;
+} Planta;
+
+
+Planta* newPlanta()
+{
+    Planta* temp = (Planta*) malloc(sizeof(Planta));
+    vector<vector<Segmento>> tempVector;
+    temp -> listaAdj = tempVector;
+
+    return temp;
+}
+
+Segmento* newSegmento(int vSaida, int vEntrada, vector<Imovel> imoveis, int limVel, int tamanho, string rua)
+{
+    Segmento* temp = (Segmento*) malloc(sizeof(Segmento));
+    
+    temp -> vSaida = vSaida;
+    temp -> vEntrada = vEntrada;
+    temp -> imoveis = imoveis;
+    temp -> limVel = limVel;
+    temp -> tamanho = tamanho;
+    temp -> rua = rua;
+
+    return temp;
+}
+
+Imovel* newImovel(int dFinalSeg, int CEP, string rua, int num, string tipo)
+{
+    Imovel* temp = (Imovel*) malloc(sizeof(Imovel));
+    
+    temp -> dFinalSeg = dFinalSeg;
+    temp -> CEP = CEP;
+    temp -> rua = rua;
+    temp -> num = num;
+    temp -> tipo = tipo;
+
+    return temp;
+}
+
 
 int main(){
     return 0;
