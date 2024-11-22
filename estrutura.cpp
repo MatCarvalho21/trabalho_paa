84a24c02e99d8435f3ce52f5bae3561338229d17
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-11-22 18:14:34 -0300

    Terminando as structs

--- estrutura.cpp
+++ estrutura.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cstdlib>
 
 using std::cout;
 using std::string;
 using std::endl;
-
-using namespace std;
+using std::vector;
 
 // Estrutura do imóvel
-typedef struct Imovel{
+typedef struct Imovel
+{
     // Distância até o final do segmento a que pertence
     int dFinalSeg;
     int CEP;
@@ -20,63 +19,98 @@ typedef struct Imovel{
 } Imovel;
 
 // Estrutura do segmento (aresta do grafo)
-typedef struct Segmento{
+typedef struct Segmento
+{
     // Vértices de onde ele sai e entra
     int vSaida;
     int vEntrada;
     // Vetor dos imóveis
-    vector<Imovel> imoveis;
+    vector<Imovel*> imoveis;
     int limVel;
     int tamanho;
     string rua;
 } Segmento;
 
-typedef struct Planta{
+typedef struct Planta
+{
     // A lista de adjacência é um vetor de vetores de segmentos
     // Cada entrada i do vetor externo corresponde às arestas de saída do vértice i
-    vector<vector<Segmento>> listaAdj;
+    vector<vector<Segmento*> > listaAdj;
 } Planta;
 
 
-Planta* newPlanta()
+Planta* newPlanta(int numVertices)
 {
-    Planta* temp = (Planta*) malloc(sizeof(Planta));
-    vector<vector<Segmento>> tempVector;
-    temp -> listaAdj = tempVector;
-
+    Planta* temp = new Planta();
+    temp->listaAdj.resize(numVertices);
     return temp;
 }
 
-Segmento* newSegmento(int vSaida, int vEntrada, vector<Imovel> imoveis, int limVel, int tamanho, string rua)
+Segmento* newSegmento(int vSaida, int vEntrada, int limVel, int tamanho, string rua)
 {
-    Segmento* temp = (Segmento*) malloc(sizeof(Segmento));
+    Segmento* temp = new Segmento();
     
-    temp -> vSaida = vSaida;
-    temp -> vEntrada = vEntrada;
-    temp -> imoveis = imoveis;
-    temp -> limVel = limVel;
-    temp -> tamanho = tamanho;
-    temp -> rua = rua;
+    temp->vSaida = vSaida;
+    temp->vEntrada = vEntrada;
+
+    vector<Imovel*> tempImoveis;
+    temp->imoveis = tempImoveis;
+
+    temp->limVel = limVel;
+    temp->tamanho = tamanho;
+    temp->rua = rua;
 
     return temp;
 }
 
 Imovel* newImovel(int dFinalSeg, int CEP, string rua, int num, string tipo)
 {
-    Imovel* temp = (Imovel*) malloc(sizeof(Imovel));
+    Imovel* temp = new Imovel();
     
-    temp -> dFinalSeg = dFinalSeg;
-    temp -> CEP = CEP;
-    temp -> rua = rua;
-    temp -> num = num;
-    temp -> tipo = tipo;
+    temp->dFinalSeg = dFinalSeg;
+    temp->CEP = CEP;
+    temp->rua = rua;
+    temp->num = num;
+    temp->tipo = tipo;
 
     return temp;
 }
 
+void adicionaImovelASegmento(Imovel* imovel, Segmento* segmento)
+{
+    segmento -> imoveis.push_back(imovel);
+}
 
-int main(){
-    return 0;
+void adicionaSegmentoAPlanta(Segmento* segmento, Planta* planta)
+{
+    ((planta -> listaAdj)[segmento -> vSaida]).push_back(segmento);
 }
 
 
+int main()
+{
+    Imovel* teste = newImovel(3, 20, "rua", 30, "com");
+    Segmento* steste = newSegmento(1, 2, 50, 30, "rua");
+    Planta* pteste = newPlanta(5);
+
+    // cout << "Imovel: " << endl;
+    // cout << "dFinalSeg: " << teste->dFinalSeg << endl;
+    // cout << "CEP: " << teste->CEP << endl;
+    // cout << "Rua: " << teste->rua << endl;
+    // cout << "Número: " << teste->num << endl;
+    // cout << "Tipo: " << teste->tipo << endl;
+
+    adicionaImovelASegmento(teste, steste);
+
+    cout << steste->vSaida << endl;
+
+    adicionaSegmentoAPlanta(steste, pteste);
+
+    // printListaAdj(pteste);
+
+    cout << (((pteste -> listaAdj)[1])[0]) -> rua;
+
+    delete teste; // Liberando a memória alocada
+
+    return 0;
+}
