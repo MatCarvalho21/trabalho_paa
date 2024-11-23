4a18126a1fd7300310511dbed8d181a673cbce51
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-11-23 17:04:59 -0300

    Ajuste nos protótipos

--- mapa.h
+++ mapa.h
@@ -6,9 +6,10 @@
 #ifndef MAPA_H
 #define MAPA_H
 
-int stringToInt(const std::string& str);
-string removeChar(const string& str, char ch);
-vector<string> split(const string& str, char delimiter);
-void carregaJSON(const string& caminhoArquivo, Planta* planta);
+int stringToInt(const std::string&);
+string trim(const string&);
+string removeQuotes(const string&);
+string extractValue(const string&);
+void carregaJSON(const string&, Planta*);
 
 #endif
