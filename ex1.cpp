45d067ed944ba5c81c31dd167f574e9d24be9492
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-11-24 15:52:38 -0300

    Criando o módulo do algoritmo do exercício 1

---
+++ ex1.cpp
@@ -0,0 +1,12 @@
+#include "estrutura.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<Segmento*> subway(Planta* planta)
+{
+
+}
---
+++ ex1.h
@@ -0,0 +1,13 @@
+/**
+ * @file ex1.h
+ * @brief Módulo para definição da função do algoritmo do problema 1.
+ */
+
+#ifndef EX1_H
+#define EX1_H
+
+#include "estrutura.h"
+
+vector<Segmento*> subway(Planta*);
+
+#endif
